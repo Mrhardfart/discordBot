f53d3e71c2ddd98758c9c69c5bf5cf6d64b275a6
Mrhardfart <mrhardfart7889@gmail.com>
2026-08-21 14:22:58 +0200

    Create upd_log.py

---
+++ upd_log.py
@@ -0,0 +1,71 @@
+UPD_LOG = {
+    "v1.6": {
+        "date": "<t:1783518060:R>",
+        "additions": [
+            "**NEW ROLE + BALANCE CHANGES**",
+            "<:TapuBulu:1524114395263926332> New role: Tapu Bulu!",
+            ":sparkles: New jackpot announcements channel!",
+            "<:downvote:1524028073295745164> /work's gains have been nerfed to `75-150`. With the introduction of new roles boosting these gains, other minigames have been overshadowed by `/work` so this change will allow other minigames to be more relevant. Encouraging more gambling and less farming with no risks.",
+            "<:upvote:1524028103179894784> Level XP step: `10` --> `8`. (It is easier to level up now)",
+        ],
+        "footer": "Use code `TAPUBULU` for freebies!"
+    },
+    "v1.5": {
+        "date": "<t:1783427160:R>",
+        "additions": [
+            "**POKEMON CRATE! <:PokemonCrate:1523436005234053342>**",
+            ":sparkles: 10 New roles to collect! Use /roles index to view them all.",
+            ":money_mouth: Added up to top 50 leaderboard to `/leaderboard` command!",
+            "<:downvote:1524028073295745164> North Star crate odds: `2` --> `1.8`",
+            "<a:pepedance:1523412344879710391> New max. level milestone: 35.",
+            "<:upvote:1524028103179894784> New DM notifications when receiving money from other members.",
+            ":robot: Bot rate-limiting improvements.",
+            "<:heart:1516161504561598554> New command: /crate view",
+        ],
+        "footer": "Use code `POKECRATES1` and `LVL35` for freebies!"
+    },
+    "v1.42": {
+        "date": "<t:1783208520:R>",
+        "additions": [
+            "**BALANCE CHANGES + NEW ROLE!**",
+            "<:d_arrow:1522026441984704672> New role: North Star! Boosts the chances of obtaining any crate by 2x!",
+            "<:d_arrow:1522026441984704672> Buffed Seraphim: Boosts work and crime earnings by 75% (up from 65%)!",
+            "<:d_arrow:1522026441984704672> Buffed Forsaken Riftide: Boosts work and crime earnings by 50% (up from 35%)!",
+        ],
+        "footer": "Use code `NORTHSTAR` for freebies!"
+    },
+    "v1.41": {
+        "date": "<t:1783164360:R>",
+        "additions": [
+            "<:d_arrow:1522026441984704672> You can now open multiple crates at once! (/open [crate] [amount])",
+            "<:d_arrow:1522026441984704672> Buffed bank caps!",
+            "<:d_arrow:1522026441984704672> Guess the number rework. Guess went from `1-99` --> `1-25`.",
+        ],
+        "footer": "Use code `GUESSTHENUMBERFIX2` for freebies!"
+    },
+    "v1.4": {
+        "date": "<t:1783159740:R>",
+        "additions": [
+            "<:d_arrow:1522026441984704672> Reworked Word Scramper!",
+            "<:d_arrow:1522026441984704672> Reworked Guess the number!",
+        ],
+        "footer": "CODE EXPIRED"
+    },
+    "v1.3": {
+        "date": "<t:1783119060:R>",
+        "additions": [
+            "<:d_arrow:1522026441984704672> New minigame: Word Scramper!",
+            "<:d_arrow:1522026441984704672> New minigame: Guess the number!",
+            "Both minigames appear randomly in <#1516160494971519184> every 10 minutes, and you have 1 minute to participate!",
+        ],
+        "footer": "CODE EXPIRED"
+    },
+    "v1.2": {
+        "date": "<t:1783105200:R>",
+        "additions": [
+            "<:d_arrow:1522026441984704672> Angelic crates from `/work`: 3% --> 5%",
+            "<:d_arrow:1522026441984704672> Loot Crates can now drop `x1 Angelic Crate` at 3%, and `x3 Angelic Crates` at 1%",
+        ],
+        "footer": "CODE EXPIRED"
+    }
+}
