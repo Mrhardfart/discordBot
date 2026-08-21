8fa719dcf184ab79ff3cc657536e1a42d3480fde
Mrhardfart <mrhardfart7889@gmail.com>
2026-08-21 14:21:37 +0200

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,56 @@
+CODES_TABLE = {
+    "TAPUBULU" : {
+        "rewards": {
+            "won_currency": 5000,
+            "stardust_currency": 40,
+            "crates": {
+                "pokemon_crate": 3,
+                "angelic_crate": 1,
+            }
+        },
+        "max_redeems": 1,
+    },
+    "LVL35" : {
+        "rewards": {
+            "won_currency": 5000,
+            "stardust_currency": 40,
+            "crates": {
+                "pokemon_crate": 3,
+                "angelic_crate": 1,
+            }
+        },
+        "max_redeems": 1,
+    },
+    "POKECRATES1" : {
+        "rewards": {
+            "won_currency": 5000,
+            "stardust_currency": 40,
+            "crates": {
+                "pokemon_crate": 3,
+            }
+        },
+        "max_redeems": 1,
+    },
+    "NORTHSTAR" : {
+        "rewards": {
+            "won_currency": 10000,
+            "stardust_currency": 67,
+            "crates": {
+                "loot_crate": 15,
+                "angelic_crate": 2,
+            }
+        },
+        "max_redeems": 1,
+    },
+    "GUESSTHENUMBERFIX2" : {
+        "rewards": {
+            "won_currency": 10000,
+            "stardust_currency": 30,
+            "crates": {
+                "loot_crate": 10,
+                "angelic_crate": 3,
+            }
+        },
+        "max_redeems": 1,
+    },
+}
