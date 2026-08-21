edaf687cd3a3c7c0d57939ec7cfcbe08478d0cce
Mrhardfart <mrhardfart7889@gmail.com>
2026-08-21 14:22:36 +0200

    Create roles.py

---
+++ roles.py
@@ -0,0 +1,290 @@
+
+ROLES_TABLE = {
+    "management_cosmetic": {
+        "display_name": "Management",
+        "role_id": 1522669892703355112,
+        "rarity": "Unique",
+        "hidden": True,
+        "description": "Cosmetic role for Management!",
+    },
+    "moderator_cosmetic": {
+        "display_name": "Moderator",
+        "role_id": 1522670168323391508,
+        "rarity": "Unique",
+        "hidden": True,
+        "description": "Cosmetic role for Moderators!",
+    },
+    "jr_moderator_cosmetic": {
+        "display_name": "Junior Moderator",
+        "role_id": 1522670404181692446,
+        "rarity": "Unique",
+        "hidden": True,
+        "description": "Cosmetic role for Junior Moderators!",
+    },
+    "ultimate_champion": {
+        "display_name": "Ultimate Champion",
+        "role_id": 1517138093596213308,
+        "rarity": "Unique",
+        "hidden": True,
+        "description": "Top 3 Clan points!",
+    },
+    "clan_supporter": {
+        "display_name": "Clan Supporter",
+        "role_id": 1516905953335967854,
+        "rarity": "Unique",
+        "hidden": True,
+        "description": "[UNOBTAINABLE] Boosted the private server 3 times or more!",
+    },
+    "candy_blossom": {
+        "display_name": "Candy Blossom",
+        "role_id": 1522014174572711936,
+        "role_icon": "<:candy_blossom_icon:1522016041407086732>",
+        "rarity": "Common",
+        "description": "A sweet treat for your profile!",
+    },
+    "leaf_village_heir": {
+        "display_name": "Leaf Village Heir",
+        "role_id": 1522014253735870464,
+        "role_icon": "<:leaf_icon:1522016083697991720>",
+        "rarity": "Common",
+        "description": "A prestigious role for your profile!",
+    },
+    "storm_chaser": {
+        "display_name": "Storm Chaser",
+        "role_id": 1522014335629525033,
+        "role_icon": "<:lightningicon:1522016100785586297>",
+        "rarity": "Common",
+        "description": "A daring role for your profile!",
+    },
+    "rose_thorn": {
+        "display_name": "Rose Thorn",
+        "role_id": 1522014416470806558,
+        "role_icon": "<:roseicon:1522016123699073215>",
+        "rarity": "Rare",
+        "description": "A thorny role for your profile!",
+    },
+    "moonlight_sage": {
+        "display_name": "Moonlight Sage",
+        "role_id": 1522014535777783848,
+        "role_icon": "<:moonicon:1522016154946900008>",
+        "rarity": "Rare",
+        "description": "A mystical role for your profile!",
+    },
+    "ocean_stardust": {
+        "display_name": "Ocean Stardust",
+        "role_id": 1522014484695220294,
+        "role_icon": "<:waveicon:1522016179147768038>",
+        "rarity": "Epic",
+        "description": "A deep-sea role for your profile!",
+    },
+    "cyber_soul": {
+        "display_name": "Cyber Soul",
+        "role_id": 1522014625695273080,
+        "role_icon": "<:chipicon:1522016205664288850>",
+        "rarity": "Epic",
+        "description": "A futuristic role for your profile!",
+    },
+    "dragons_breath": {
+        "display_name": "Dragons Breath",
+        "role_id": 1522014719685296219,
+        "role_icon": "<:dragonicon:1522016234370109461>",
+        "rarity": "Legendary",
+        "description": "A fiery role for your profile!",
+    },
+    "void_walker": {
+        "display_name": "Void Walker",
+        "role_id": 1522014805869990078,
+        "role_icon": "<:voidicon:1522016259645112331>",
+        "rarity": "Legendary",
+        "description": "A mysterious role for your profile!",
+    },
+    "spirit_blade": {
+        "display_name": "Spirit Blade",
+        "role_id": 1522014871129030797,
+        "role_icon": "<:swordsicon:1522016284311814195>",
+        "rarity": "Legendary",
+        "description": "A legendary role for your profile!",
+    },
+    "oceanic_reaper": {
+        "display_name": "Oceanic Reaper",
+        "role_id": 1522235037909651526,
+        "role_icon": "<:OceanicReaper:1522217441445548184>",
+        "rarity": "Rare",
+        "description": "A fearsome role for your profile!",
+    },
+    "kraken": {
+        "display_name": "Kraken",
+        "role_id": 1522236507317075988,
+        "role_icon": "<:Kraken:1522217415960957019>",
+        "rarity": "Common",
+        "description": "A mighty role for your profile!",
+    },
+    "forsaken_riftide": {
+        "display_name": "Forsaken Riftide",
+        "role_id": 1520753815601545257,
+        "role_icon": "<:Forsaken_Riftide:1522217565781495839>",
+        "rarity": "Legendary",
+        "boosts": {
+            "work": 1.50,
+            "crime": 1.50,
+        },
+        "description": "Boosts Work and Crime earnings by 50%!",
+    },
+    "kitty_launcher": {
+        "display_name": "Kitty Launcher",
+        "role_id": 1522266009954812076,
+        "rarity": "Unique",
+        "show_copies": True,
+        "boosts": {
+            "blackjack": 1.2,
+            "coinflip": 1.2,
+            "crash": 1.2,
+            "roulette": 1.2,
+            "slots": 1.2,
+            "work": 1.5,
+            "crime": 1.5,
+        },
+        "description": "Boosts all casino games by 20%, and boosts work and crime earnings by 50%! Used to be obtainable through Butterfly Crate.",
+        "role_icon": "<:kittyLauncher:1522266215064666132>"
+    },
+    "seraphim": {
+        "display_name": "Seraphim",
+        "role_id": 1522646595538128916,
+        "rarity": "Unique",
+        "show_copies": True,
+        "boosts": {
+            "work": 1.75,
+            "crime": 1.75,
+        },
+        "description": "Boosts work and crime earnings by 75%! Obtainable through Angelic Crate.",
+        "role_icon": "<:Seraphim:1522217404606840982>"
+    },
+    "north_star": {
+        "display_name": "North Star",
+        "role_id": 1523113250391064816,
+        "rarity": "Unique",
+        "show_copies": True,
+        "boosts": {
+            "crate_odds": 1.8,
+        },
+        "description": "Boosts your chances of obtaining crates by 1.8x!",
+        "role_icon": "<:North_Star:1522217556226736180>"
+    },
+    "sunkern": {
+        "display_name": "Sunkern",
+        "role_id": 1522327064286597361,
+        "rarity": "Rare",
+        "boosts": {
+            "work": 1.25,
+        },
+        "description": "Boosts work earnings by 25%!",
+        "role_icon": "<:Sunkern:1523431300109697154>"
+    },
+    "magikarp": {
+        "display_name": "Magikarp",
+        "role_id": 1523428249944719431,
+        "rarity": "Rare",
+        "boosts": {
+            "work": 1.25,
+            "crime": 1.15,
+        },
+        "description": "Boosts work and crime earnings by 25% and 15% respectively!",
+        "role_icon": "<:Magikarp:1523431333345492993>"
+    },
+    "smeargle": {
+        "display_name": "Smeargle",
+        "role_id": 1523428739591962674,
+        "rarity": "Rare",
+        "boosts": {
+            "work": 1.30,
+            "crime": 1.15,
+        },
+        "description": "Boosts work and crime earnings by 30% and 15% respectively!",
+        "role_icon": "<:Smeargle:1523431356581806393>"
+    },
+    "ledian": {
+        "display_name": "Ledian",
+        "role_id": 1523429061542674654,
+        "rarity": "Rare",
+        "boosts": {
+            "work": 1.20,
+            "crime": 1.25,
+        },
+        "description": "Boosts work and crime earnings by 20% and 25% respectively!",
+        "role_icon": "<:Ledian:1523431389129605279>"
+    },
+    "quagsire": {
+        "display_name": "Quagsire",
+        "role_id": 1523429218707570780,
+        "rarity": "Epic",
+        "boosts": {
+            "work": 1.40,
+            "crime": 1.30,
+        },
+        "description": "Boosts work and crime earnings by 40% and 30% respectively!",
+        "role_icon": "<:Quagsire:1523431451981385978>"
+    },
+    "incineroar": {
+        "display_name": "Incineroar",
+        "role_id": 1523429327600095340,
+        "rarity": "Legendary",
+        "boosts": {
+            "work": 1.45,
+            "crime": 1.35,
+            "crate_odds": 1.15,
+        },
+        "description": "Boosts work by 45%, crime by 35%, and crate odds by 15%!",
+        "role_icon": "<:Incineroar:1523431516267483296>"
+    },
+    "garchomp": {
+        "display_name": "Garchomp",
+        "role_id": 1523429508357554176,
+        "rarity": "Legendary",
+        "boosts": {
+            "work": 1.50,
+            "crime": 1.45,
+            "crate_odds": 1.20,
+        },
+        "description": "Boosts work by 50%, crime by 45%, and crate odds by 20%!",
+        "role_icon": "<:Garchomp:1523431537021026414>"
+    },
+    "iron_valliant": {
+        "display_name": "Iron Valiant",
+        "role_id": 1523430203655979049,
+        "rarity": "Legendary",
+        "boosts": {
+            "work": 1.60,
+            "crime": 1.60,
+            "blackjack": 1.15,
+        },
+        "description": "Boosts work and crime earnings by 60%, and blackjack earnings by 15%!",
+        "role_icon": "<:IronValiant:1523431573070942239>"
+    },
+    "tapu_koko": {
+        "display_name": "Tapu Koko",
+        "role_id": 1523430414335742113,
+        "rarity": "Unique",
+        "show_copies": True,
+        "boosts": {
+            "work": 1.75,
+            "crime": 1.75,
+            "crate_odds": 1.3,
+        },
+        "description": "Boosts work and crime earnings by 75%, and crate odds by 30%!",
+        "role_icon": "<:TapuKoko:1523431608982573138>"
+    },
+    "tapu_bulu": {
+        "display_name": "Tapu Bulu",
+        "role_id": 1524409208336945316,
+        "rarity": "Unique",
+        "show_copies": True,
+        "boosts": {
+            "work": 2,
+            "crime": 1.75,
+            "crate_odds": 1.25,
+        },
+        "description": "Boosts work earnings by 100%, crime earnings by 75%, and crate odds by 25%!",
+        "role_icon": "<:TapuBulu:1524114395263926332>"
+    },
+    
+}
