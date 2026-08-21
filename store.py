68d6e9cb392c6a25e49165e7b6e4a92714bb793d
Mrhardfart <mrhardfart7889@gmail.com>
2026-08-21 14:22:47 +0200

    Create store.py

---
+++ store.py
@@ -0,0 +1,118 @@
+STORE_ITEMS_BY_GUILD = {
+    1521967226918277161: [
+        {
+            "id": "leaf_role",
+            "name": "Leaf Role",
+            "role_id": 1521977895990333510,
+            "cost": 10,
+            "display_emoji": "<:role_icon:1521980337003958508>",
+        },
+        {
+            "id": "fire_role",
+            "name": "Fire Role",
+            "role_id": 1521984362252599476,
+            "cost": 25,
+            "display_emoji": "<:role_icon:1521980337003958508>",
+        }
+    ],
+    1516160494082330705: [
+        {
+            "id": "candy_blossom",
+            "name": "Candy Blossom",
+            "role_id": 1522014174572711936,
+            "cost": 1000,
+            "display_emoji": "<:candy_blossom_icon:1522016041407086732>",
+        },
+        {
+            "id": "leaf_village_heir",
+            "name": "Leaf Village Heir",
+            "role_id": 1522014253735870464,
+            "cost": 1000,
+            "display_emoji": "<:leaf_icon:1522016083697991720>",
+        },
+        {
+            "id": "storm_chaser",
+            "name": "Storm Chaser",
+            "role_id": 1522014335629525033,
+            "cost": 1000,
+            "display_emoji": "<:lightningicon:1522016100785586297>",
+        },
+        {
+            "id": "kraken",
+            "name": "Kraken",
+            "role_id": 1522236507317075988,
+            "cost": 5000,
+            "display_emoji": "<:Kraken:1522217415960957019>",
+        },
+        {
+            "id": "rose_thorn",
+            "name": "Rose Thorn",
+            "role_id": 1522014416470806558,
+            "cost": 7000,
+            "display_emoji": "<:roseicon:1522016123699073215>",
+        },
+        {
+            "id": "moonlight_sage",
+            "name": "Moonlight Sage",
+            "role_id": 1522014535777783848,
+            "cost": 7000,
+            "display_emoji": "<:moonicon:1522016154946900008>",
+        },
+        {
+            "id": "oceanic_reaper",
+            "name": "Oceanic Reaper",
+            "role_id": 1522235037909651526,
+            "cost": 9000,
+            "display_emoji": "<:OceanicReaper:1522217441445548184>",
+        },
+        {
+            "id": "ocean_stardust",
+            "name": "Ocean Stardust",
+            "role_id": 1522014484695220294,
+            "cost": 10000,
+            "display_emoji": "<:waveicon:1522016179147768038>",
+        },
+        {
+            "id": "cyber_soul",
+            "name": "Cyber Soul",
+            "role_id": 1522014625695273080,
+            "cost": 20000,
+            "display_emoji": "<:chipicon:1522016205664288850>",
+        },
+        {
+            "id": "dragons_breath",
+            "name": "Dragons Breath",
+            "role_id": 1522014719685296219,
+            "cost": 25000,
+            "display_emoji": "<:dragonicon:1522016234370109461>",
+        },
+        {
+            "id": "void_walker",
+            "name": "Void Walker",
+            "role_id": 1522014805869990078,
+            "cost": 45000,
+            "display_emoji": "<:voidicon:1522016259645112331>",
+        },
+        {
+            "id": "spirit_blade",
+            "name": "Spirit Blade",
+            "role_id": 1522014871129030797,
+            "cost": 50000,
+            "display_emoji": "<:swordsicon:1522016284311814195>",
+        },
+        {
+            "id": "forsaken_riftide",
+            "name": "Forsaken Riftide",
+            "role_id": 1520753815601545257,
+            "cost": 55000,
+            "display_emoji": "<:Forsaken_Riftide:1522217565781495839>",
+        },
+        {
+            "id": "north_star",
+            "name": "North Star",
+            "role_id": 1523113250391064816,
+            "cost": 100000,
+            "display_emoji": "<:North_Star:1522217556226736180>",
+        }
+    ]
+}
